641a23d4648049062e00e7c000c5663fa233b4e0
joelstewart <joel@codeinversion.com>
2020-12-11 10:49:44 -0600

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLink",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v11),
+        .tvOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SwiftLink",
+            targets: ["SwiftLink"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SwiftLink",
+            path: "SwiftLink",
+            cxxSettings: [
+                .headerSearchPath("../link/include"),
+                .headerSearchPath("../link/modules/asio-standalone/asio/include"),
+                .define("LINK_PLATFORM_MACOSX")
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .gnucxx14
+)
